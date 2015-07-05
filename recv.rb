a19d01a4a54eb3fcaae3f563e3310235c5f0da13
Kieran Trezona-le Comte <trezona-lecomte@gmail.com>
2015-07-06 11:39:45 +1200

    Fixed version, crudely transferring

--- recv.rb
+++ recv.rb
@@ -1,15 +1,15 @@
 #!/usr/bin/env ruby
-puts "++running server..."
+# require 'pry'
 
 @in = STDIN.read
 puts "++file recd on server: \n#{@in}"
 
-begin
-  ARGF.each do |file|
-    File.open(file, 'a+') { |f| f.write(@in) }
-    puts "++existing file written from server."
-  end
-rescue Errno::ENOENT
+if File.exists?(ARGF.filename)
   File.open(ARGF.filename, 'a+') { |f| f.write(@in) }
+
+  puts "++existing file written from server."
+else
+  File.open(ARGF.filename, 'a+') { |f| f.write(@in) }
+
   puts "++new file created and written from server."
 end
--- send.rb
+++ send.rb
@@ -1,16 +1,4 @@
 #!/usr/bin/env ruby
-STDERR.puts "--running client..."
 
 STDOUT.write(ARGF.read)
 STDERR.puts "\n--file sent from client..."
-
-
-# class Client(input, output)
-#   def initialize
-#     server = Server.new
-#   end
-
-#   def send(remote_file:)
-
-#   end
-# end
