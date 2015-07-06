018d5f035c1923445195b0406eb360b3dea6667e
Kieran Trezona-le Comte <trezona-lecomte@gmail.com>
2015-07-06 15:48:34 +1200

    Working version with readlines

--- recv.rb
+++ recv.rb
@@ -2,26 +2,94 @@
 require 'stringio'
 require 'byebug'
 
-class Server
-  def initialize(input:, output: ARGF.filename)
-    @in = input
-    @out = output
-    # byebug
+class Recv
+  MB = 4
+
+  def initialize
+    begin
+      @old_output = StringIO.new(ARGF.read)
+      @new_output = File.open(ARGF.filename, "w+")
+    rescue Errno::ENOENT
+      @old_output = StringIO.new
+      @new_output = File.open(ARGF.filename, "w")
+    end
   end
 
-  def save_file
-    if File.exists?(@out)
-      File.open(@out, 'a+') { |f| f.write(@in) }
+  def receive
+    until STDIN.eof?
+      new_sum = STDIN.readline.to_i
 
-      puts "++existing file written from server."
-    else
-      File.open(@out, 'a+') { |f| f.write(@in) }
+      begin
+        old_line = @old_output.readline
+      rescue EOFError
+        old_line = ""
+      end
 
-      puts "++new file created and written from server."
+      old_sum = old_line.sum
+
+      if new_sum.to_i == old_sum.to_i
+        STDOUT.write("n")
+
+        @new_output.write(old_line)
+
+        STDERR.puts "Wrote OLD line: #{old_line}"
+      else
+        STDOUT.write("y")
+
+        new_line = STDIN.readline
+        @new_output.write(new_line)
+
+        STDERR.puts "Wrote NEW line: #{new_line}"
+      end
     end
+
+    @old_output.close
+    @new_output.close
   end
 end
 
-server = Server.new(input: "testing!!!", output: "local-file.txt")
+recv = Recv.new.receive
 
-server.save_file
+    # until !(new_chunk = STDIN.read(MB))
+    #   existing_chunk = @output.read(MB)
+
+    #   puts STDIN.read(MB).sum
+
+    #   if new_chunk == existing_chunk
+    #     puts existing_chunk
+    #   else
+    #     puts new_chunk
+    #   end
+    # end
+      # puts chunk
+      # chunk = STDIN.read(4)
+  # end
+      # File.open(@out_file, "a+") do |file|
+      #   file.write(STDIN.read)
+      # end
+    # end
+    # until STDIN.eof?
+    #   STDIN.each_byte do |byte|
+    #     puts byte
+    #   end
+    # end
+# end
+
+#   def save_file
+#     if File.exists?(@out)
+#       File.open(@out, 'a+') { |f| f.write(@in.read) }
+
+#       puts "++existing file written from server."
+#     else
+#       File.open(@out, 'a+') { |f| f.write(@in.read) }
+
+#       puts "++new file created and written from server."
+#     end
+#   end
+# end
+
+# server = Server.new(output: "local-file.txt")
+# server.send(input: StringIO.new("testing!!!"))
+
+# server.save_file
+
--- send.rb
+++ send.rb
@@ -1,4 +1,23 @@
-#!/usr/bin/env ruby
+# #!/usr/bin/env ruby
 
-STDOUT.write(ARGF.read)
-STDERR.puts "\n--file sent from client..."
+# STDOUT.write(ARGF.read)
+# STDERR.puts "\n--file sent from client..."
+require 'byebug'
+
+class Send
+  def initialize
+    @in_file = ARGF.read
+  end
+
+  def transfer
+    @in_file.each_line do |line|
+      puts line.sum
+
+      if STDIN.read(1) == "y"
+        STDOUT.write(line)
+      end
+    end
+  end
+end
+
+Send.new.transfer
