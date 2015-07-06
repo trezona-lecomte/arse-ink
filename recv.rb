a6d5a699e359c0ff351b4933bef0c934eebf558c
Kieran Trezona-le Comte <trezona.lecomte@gmail.com>
2015-07-06 21:36:49 +1200

    working with chunks

--- recv.rb
+++ recv.rb
@@ -1,9 +1,12 @@
 #!/usr/bin/env ruby
 require 'stringio'
 require 'byebug'
+require 'zlib'
+require 'digest'
 
 class Recv
-  MB = 4
+  DIGEST_SIZE = 32
+  CHUNK_SIZE = 1024
 
   def initialize
     begin
@@ -19,33 +22,31 @@ class Recv
     recd_bytes = 0
 
     until STDIN.eof?
-      line = STDIN.readline
-      recd_bytes += line.bytes.size
-      new_sum = line.to_i
-      # new_sum = STDIN.readline.to_i
+      new_digest = STDIN.read(DIGEST_SIZE)
+      recd_bytes += new_digest.bytes.size
+      # STDERR.puts "\nnew new_digest, length: #{new_digest.bytes.size}, digest: #{new_digest}"
 
-      begin
-        old_line = @old_output.readline
-      rescue EOFError
-        old_line = ""
+      unless (old_chunk = @old_output.read(CHUNK_SIZE))
+        old_chunk = ""
       end
 
-      old_sum = old_line.sum
+      old_digest = Digest::MD5.hexdigest(old_chunk)
+      # STDERR.puts "old digest, length: #{old_digest.bytes.size}, Digest: #{old_digest}\n"
 
-      if new_sum.to_i == old_sum.to_i
+      if new_digest == old_digest
         STDOUT.write("n")
 
-        @new_output.write(old_line)
+        @new_output.write(old_chunk)
 
-        # STDERR.puts "Wrote OLD line: #{old_line}"
+        # STDERR.puts "Wrote OLD chunk: #{old_chunk.size}"
       else
         STDOUT.write("y")
 
-        new_line = STDIN.readline
-        @new_output.write(new_line)
-        recd_bytes += new_line.bytes.size
+        new_chunk = STDIN.read(CHUNK_SIZE)
+        @new_output.write(new_chunk)
+        recd_bytes += new_chunk.bytes.size
 
-        # STDERR.puts "Wrote NEW line: #{new_line}"
+        # STDERR.puts "Wrote NEW chunk: #{new_chunk.size}"
       end
     end
 
@@ -57,47 +58,3 @@ class Recv
 end
 
 recv = Recv.new.receive
-
-    # until !(new_chunk = STDIN.read(MB))
-    #   existing_chunk = @output.read(MB)
-
-    #   puts STDIN.read(MB).sum
-
-    #   if new_chunk == existing_chunk
-    #     puts existing_chunk
-    #   else
-    #     puts new_chunk
-    #   end
-    # end
-      # puts chunk
-      # chunk = STDIN.read(4)
-  # end
-      # File.open(@out_file, "a+") do |file|
-      #   file.write(STDIN.read)
-      # end
-    # end
-    # until STDIN.eof?
-    #   STDIN.each_byte do |byte|
-    #     puts byte
-    #   end
-    # end
-# end
-
-#   def save_file
-#     if File.exists?(@out)
-#       File.open(@out, 'a+') { |f| f.write(@in.read) }
-
-#       puts "++existing file written from server."
-#     else
-#       File.open(@out, 'a+') { |f| f.write(@in.read) }
-
-#       puts "++new file created and written from server."
-#     end
-#   end
-# end
-
-# server = Server.new(output: "local-file.txt")
-# server.send(input: StringIO.new("testing!!!"))
-
-# server.save_file
-
--- send.rb
+++ send.rb
@@ -1,19 +1,34 @@
 # #!/usr/bin/env ruby
 require 'byebug'
+require 'zlib'
+require 'digest'
 
 class Send
+DIGEST_SIZE = 32
+CHUNK_SIZE = 1024
+
   def initialize
-    @in_file = ARGF.read
+    @in_file = ARGF.file
   end
 
   def transfer
-    @in_file.each_line do |line|
-      puts line.sum
+    chunks(@in_file).each do |chunk|
+      # puts line.sum
+      STDOUT.write(Digest::MD5.hexdigest(chunk))
 
       if STDIN.read(1) == "y"
-        STDOUT.write(line)
+        STDOUT.write(chunk)
       end
     end
+  end
+
+  def chunks(str)
+    chunks = []
+    until str.eof?
+      chunk = str.read(CHUNK_SIZE)
+      chunks << chunk
+    end
+    chunks
   end
 end
 
